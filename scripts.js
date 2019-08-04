69db0c462304e1f56b6d4b10458150366cf30bb0
Robert Lee <bobbydwise1@hotmail.com>
2019-08-03 20:32:33 -0700

    Simplify search function.

--- scripts.js
+++ scripts.js
@@ -10,32 +10,38 @@ For example, given the set of words 'quick', 'brown', 'the', 'fox', and the stri
 Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
 */
 
-const dictionary = [
-  'quick',
-  'brown',
-  'fox',
-  'jumps',
-  'over',
-  'the',
-  'lazy',
-  'dog',
-  'bed',
-  'bath',
-  'beyond',
-];
-
-let testString = 'bed';
-
 const searchTerms = (yourString, dictionary) => {
-  
+  let output = [];
+  for (i=0; i<dictionary.length; i++) {
+    if (yourString.match(dictionary[i]) != null) {
+      output.push(dictionary[i]);
+    }
+  }
+  if (output >= 0) {
+    return null;
+  } else {
+    return output;
+  }
 }
-
-console.log(matcher('bed',dictionary))
-console.log(search(testString,dictionary))
 
 $(document).ready(function() {
 
-  $('#output-section-1').text(1);
-  $('#output-section-2').text(2);
+  const dictionary = [
+    'quick',
+    'brown',
+    'fox',
+    'jumps',
+    'over',
+    'the',
+    'lazy',
+    'dog',
+    'bed',
+    'bath',
+    'beyond',
+  ];
+  let testString = 'quickbrownfoxthe';
 
+  $('#output-section-1').text(dictionary);
+  $('#output-section-2').text(testString);
+  $('#output-section-3').text(searchTerms(testString, dictionary));
 });
